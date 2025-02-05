01451bf9f5eb9fe1b58d3dfbb667a2f6e0bf9814
JohnMaxwellGomes <johnmaxwellgomes@gmail.com>
2025-02-05 15:18:47 +0600

    cteated 0-provider.tf file

---
+++ 0-provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.33"
+    }
+  }
+}
+
+# configured aws provider with proper credentials
+provider "aws" {
+  profile   = "host"
+}
