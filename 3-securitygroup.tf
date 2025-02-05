bef94550216409e8bf26c1ea178b814090173246
JohnMaxwellGomes <johnmaxwellgomes@gmail.com>
2025-02-05 15:25:37 +0600

    VPC, subnet and security group file added

---
+++ 3-securitygroup.tf
@@ -0,0 +1,26 @@
+# create security group for the Jenkins instance
+resource "aws_security_group" "security_group" {
+  name        = "default security group"
+  description = "To allow access on ports"
+  vpc_id      = aws_default_vpc.default_vpc.id
+
+  # allow access on port 8080
+  ingress {
+    description      = "ssh access"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = -1
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags   = {
+    Name = "Default security group"
+  }
+}
