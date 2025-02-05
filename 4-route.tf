a1ad6c082620e28d9dbf5c3f97d7fb1dcc43032e
JohnMaxwellGomes <johnmaxwellgomes@gmail.com>
2025-02-05 15:54:53 +0600

    new files added

---
+++ 4-route.tf
@@ -0,0 +1,12 @@
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "public_rt_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
