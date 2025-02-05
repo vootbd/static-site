b81ae41f8a88811ab1643297995957ab6009951f
JohnMaxwellGomes <johnmaxwellgomes@gmail.com>
2025-02-05 16:00:57 +0600

    availability_zones

--- 3-subnet.tf
+++ 3-subnet.tf
@@ -14,7 +14,6 @@ resource "aws_default_subnet" "default_az1" {
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.main_vpc.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
   tags = {
     Name = "public-subnet"
