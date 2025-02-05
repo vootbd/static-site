cde23b13c7cc3626e713d0956115628848ec3131
JohnMaxwellGomes <johnmaxwellgomes@gmail.com>
2025-02-05 15:49:05 +0600

    new edit

--- 1-vpc.tf
+++ 1-vpc.tf
@@ -5,3 +5,10 @@ resource "aws_default_vpc" "default_vpc" {
     Name  = "default vpc"
   }
 }
+
+resource "aws_vpc" "main_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "main-vpc"
+  }
+}
